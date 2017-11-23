080896674c54eb93ffbc599fc1b5abd962bcec47
Jorge Antônio <jorgeants@gmail.com>
2017-11-23 12:11:39 -0200

    Front-end

--- app.rb
+++ app.rb
@@ -1,11 +1,22 @@
 require 'sinatra'
 require 'sinatra/activerecord'
-# require './environments'
+require 'csv'
 
 class Purchase < ActiveRecord::Base
 end
 
-get "/" do
-  @purchases = Purchase.order("created_at DESC")
-  erb :"purchases/index"
+get '/' do
+  @purchases = Purchase.all.order('created_at DESC')
+  erb :'purchases/index'
+end
+
+post '/' do
+	content = params[:file][:tempfile].read
+	content_arr = []
+	content.each_line do |line|
+	    content_arr << line
+	end
+	puts content_arr
+  
+  erb :'purchases/index'
 end
